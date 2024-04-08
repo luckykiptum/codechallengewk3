137edc5fc20869fb04c1b3b43d30436734ba85a1
lucky <luckykiptum@gmail.com>
2024-04-08 14:51:25 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
     getFilms();
 });
+let image = document.getElementById('poster');
+
+let title = document.getElementById('title')
+
+let runtime = document.getElementById('runtime')
+
+
+let showtime = document.getElementById('showtime')
+
+
+let description = document.getElementById('film-info')
+
+let movieButton=document.getElementById("btn")
+
+let movieCapacity=document.getElementById("capacity")
+
+
+let movieSoldTicket=document.getElementById("sold")
+ 
+let availableTickets = document.getElementById("ticket-num")
+
 
 function getFilms() {
     let filmsList = document.getElementById('films');
@@ -8,9 +29,29 @@ function getFilms() {
     fetch("http://localhost:3000/films")
         .then(res => res.json())
         .then((data) => {
+        const firstObj= data[0]
+        title.textContent = firstObj.title
+
+        const firstObj1= data[0]
+        runtime.textContent = firstObj1.runtime
+
+        const firstObj2= data[0]
+        showtime.textContent = firstObj2.showtime
+
+        const firstObj3 =data[0]
+        description.textContent = firstObj3.description
+
+        const firstObj4 = data[0]
+        image.src = firstObj4.poster
+
+
             data.forEach(film => {
+
                 let list = document.createElement('li');
                 list.innerText = film.title;
+
+
+
 
                 list.addEventListener('click', () => {
                      getFilmDetails(film);
@@ -26,40 +67,31 @@ function getFilms() {
 
 function getFilmDetails(film){
 
-let image = document.getElementById('poster');
+
 image.src =film.poster;
 
 
-let title = document.getElementById('title')
+
 title.innerText = `${film.title}`;
 
-let runtime = document.getElementById('runtime')
+
 runtime.innerText = `${film.runtime}`;
 
-let showtime = document.getElementById('showtime')
+
 showtime.innerText = `${film.showtime}`;
 
 
-let description = document.getElementById('film-info')
 description.innerText = `${film.description}`;
 
-let movieButton=document.getElementById("btn")
-let movieCapacity=document.getElementById("capacity")
+
 movieCapacity.innerText = `${film.capacity}`;
 
-let movieSoldTicket=document.getElementById("sold")
+
 movieSoldTicket.innerText = `${film.tickets_sold}`;
 
 
-
-
+availableTickets = movieCapacity-movieSoldTicket;
 
 }
-
-
-
-
-
-
 
 
